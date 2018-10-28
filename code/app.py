8a4b2155fd7ec43690952bd3f4670660eeb92c5a
Hossein Seyedmehdi <hosseins@surveymonkey.com>
2018-10-28 16:40:27 -0400

    Section 4 completed \n https://surveymonkey.udemy.com/rest-api-flask-and-python

--- code/app.py
+++ code/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 import sys
 sys.path.append('/Users/hosseins/Developer/programming_practice/flask_app/venv/lib/python3.6/site-packages')
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
@@ -21,12 +21,20 @@ items = []
 
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        'price',
+        type=float,
+        required=True,
+        help="This field can not be left blank!"
+    )
+
     @jwt_required()
     def get(self, name):
         """
         Test:
         curl localhost:5000/item/piano \
-            -H "Authorization: JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NDA2ODI4ODAsImlhdCI6MTU0MDY4MjU4MCwibmJmIjoxNTQwNjgyNTgwLCJpZGVudGl0eSI6MX0.xHvwsd6-Ct1cT7Sf_XtqVS8PEDJD8OhW4fJ2H4XfdxE"
+            -H "Authorization: JWT eyJ0eXAiOi...J2H4XfdxE"
         """
         item = next(filter(lambda x: x['name'] == name, items), None)
         return item
@@ -41,13 +49,33 @@ class Item(Resource):
         """
         if next(filter(lambda x: x["name"] == name, items), None):
             return {"message": f"'{name}' already exits!"}, 400
-        data = request.get_json()
+
+        # data = request.get_json()
+        data = Item.parser.parse_args()
+
         item = {
             "name": name,
             "price": data.get("price")
         }
         items.append(item)
         return item, 201
+
+    def put(self, name):
+        data = Item.parser.parse_args()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item:
+            item.update(data)
+        else:
+            item = {
+                "name": name,
+                "price": data['price']
+            }
+            items.append(item)
+
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {"message": f"Item {name} deleted."}
 
 
 class Items(Resource):
